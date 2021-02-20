97ae1c25aec925aa9b91464ea19c55e85e4b7040
Sergey Kuznetsov <skuzn99@ya.ru>
2021-02-20 12:33:59 +0300

    Revert local

--- lua/plugins/local.lua
+++ lua/plugins/local.lua
@@ -1,10 +1,4 @@
 local patch = function(default)
-    default['lsp_cmd']['clangd'] = {
-        "clangd",
-        "--background-index",
-        "-j=1",
-        "--header-insertion=never"
-    }
     return default
 end
 return patch
