a7150114c601429a7d7c6a996f4925c78cc56b70
Sergey Kuznetsov <kuzzz99@gmail.com>
2021-03-18 23:41:33 +0300

    Add open terminal in current directory

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,13 +11,15 @@ end
 nnoremap('<C-k>', ':bnext<CR>')
 nnoremap('<C-j>', ':bprev<CR>')
 
-nnoremap('<C-t>', ':split | resize 20 | terminal<CR>')
+nnoremap('<A-t>', ':split | resize 20 | terminal<CR>')
+nnoremap('<C-t>', '<Esc>:exe \'split | resize 20 | terminal bash -c "cd \' . expand("%:p:h") . \' && bash"\'<CR>')
 
 nnoremap('<C-h>', '<C-o>')
 nnoremap('<C-l>', '<C-i>')
 
 inoremap('<C-l>', '<C-^>')
-inoremap('<C-t>', '<Esc>:split | resize 20 | terminal<CR>')
+inoremap('<A-t>', '<Esc>:split | resize 20 | terminal<CR>')
+inoremap('<C-t>', '<Esc>:exe \'split | resize 20 | terminal bash -c "cd \' . expand("%:p:h") . \' && bash"\'<CR>')
 vim.api.nvim_set_keymap('t', '<Esc>', '(&filetype == "fzf") ? "<Esc>" : "<C-\\><C-n>"', {noremap=true, expr=true, silent=true})
 
 vim.cmd("command LspLog :lua vim.cmd('e'..vim.lsp.get_log_path())")
