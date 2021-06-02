f7704d5e551bebf7c1d3e11dcb5f499a6d2d80c7
Sergey Kuznetsov <kuzzz99@gmail.com>
2021-06-02 23:27:49 +0300

    Make config to reproduce bug

--- init.lua
+++ init.lua
@@ -1,5 +1,227 @@
-require('settings')
-require('colors')
-require('plugins')
-require('mappings')
-require('autocmd')
+vim.opt.termguicolors = true
+
+local function packer_bootstrap()
+    local execute = vim.api.nvim_command
+    local fn = vim.fn
+
+    local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+    if fn.empty(fn.glob(install_path)) > 0 then
+        execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+        execute 'packadd packer.nvim'
+    end
+    vim.cmd 'packadd packer.nvim'
+end
+packer_bootstrap()
+
+local function plugins_install()
+    local use = require('packer').use
+    require('packer').startup( function()
+        use { 'wbthomason/packer.nvim', opt = true }
+        -- LSP
+        use 'neovim/nvim-lspconfig'
+        use { 'hrsh7th/nvim-compe',
+            config = function()
+                vim.o.completeopt = "menuone,noinsert,noselect"
+                vim.o.shortmess = vim.o.shortmess .. 'c'
+                require'compe'.setup {
+                    documentation = true,
+                    autocomplete = true,
+                        source = {
+                            path = true;
+                            buffer = true;
+                            calc = false;
+                            vsnip = false;
+                            nvim_lsp = true;
+                            nvim_lua = true;
+                            spell = true;
+                            tags = true;
+                            snippets_nvim = false;
+                            treesitter = false;
+                        }
+                }
+            end
+        }
+        use { 'glepnir/lspsaga.nvim',
+            config = function()
+                require 'lspsaga'.init_lsp_saga {
+                    use_saga_diagnostic_sign = true,
+                    error_sign = '',
+                    warn_sign = '',
+                    hint_sign = '',
+                    infor_sign = '',
+                    code_action_icon = ' ',
+                    code_action_keys = { quit = '<Esc>',exec = '<CR>' },
+                    finder_action_keys = {
+                        open = '<CR>', vsplit = 's', split = 'i',
+                        quit = '<Esc>', scroll_down = '<C-f>',
+                        scroll_up = '<C-b>' -- quit can be a table
+                    },
+                    rename_action_keys = {
+                        quit = '<Esc>', exec = '<CR>'  -- quit can be a table
+                    },
+                    max_preview_lines = 16,
+                    definition_preview_icon = '  ',
+                    border_style = 'round'
+                }
+            end,
+        }
+        use { 'nvim-lua/lsp-status.nvim',
+            config = function()
+                require('lsp-status').register_progress()
+            end
+        }
+        -- Syntax hightlight
+        use { 'nvim-treesitter/nvim-treesitter',
+            run = function() vim.cmd('TSUpdate') end,
+            config = function()
+                require'nvim-treesitter.configs'.setup {
+                    ensure_installed = "maintained",
+                    --ignore_install = { "ocamllex", 'devicetree',
+                     --   'gdscript', 'elixir'},
+                    highlight = { enable = true }
+                }
+            end
+        }
+        -- Vim wiki
+        use { 'vimwiki/vimwiki',
+            config = function()
+                vim.g.vimwiki_list = {
+                    {path = '~/Documents/vimwiki/', syntax = 'markdown', ext = '.md'}
+                }
+            end
+        }
+    -- TODO use this plugin
+    --        use {'lervag/wiki.vim',
+    --           config = {function()
+    --              vim.g.wiki_root = '~/Documents/vimwiki/'
+    --             vim.g.wiki_extension = '.md'
+    --        end }
+    --   }
+
+        -- Improve quickfix
+        use 'kevinhwang91/nvim-bqf'
+        -- Close buffer
+        use 'ojroques/nvim-bufdel'
+
+    -- colorschemes
+        use 'michalbachowski/vim-wombat256mod'
+        use { 'tjdevries/gruvbuddy.nvim',
+            requires = { 'tjdevries/colorbuddy.nvim' }
+        }
+        use 'glepnir/zephyr-nvim'
+        use 'kuznetsss/meadow-nvim'
+
+        use { 'nvim-treesitter/playground',
+            config = function()
+                require "nvim-treesitter.configs".setup {
+                  playground = {
+                    enable = true,
+                    disable = {},
+                    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+                    persist_queries = false, -- Whether the query persists across vim sessions
+                    keybindings = {
+                      toggle_query_editor = 'o',
+                      toggle_hl_groups = 'i',
+                      toggle_injected_languages = 't',
+                      toggle_anonymous_nodes = 'a',
+                      toggle_language_display = 'I',
+                      focus_language = 'f',
+                      unfocus_language = 'F',
+                      update = 'R',
+                      goto_node = '<cr>',
+                      show_help = '?',
+                    },
+                  }
+                }
+            end
+        }
+
+
+    -- Draw colors of codes
+        use { 'norcalli/nvim-colorizer.lua',
+            config = function()
+                require'colorizer'.setup({'*'}, { names = false })
+            end
+        }
+    -- Icons
+        use 'kyazdani42/nvim-web-devicons'
+
+    -- Indent draw
+        use { 'lukas-reineke/indent-blankline.nvim',
+            branch = 'lua',
+        }
+    -- Status line
+        use { 'datwaft/bubbly.nvim',
+            config = function()
+                vim.g.bubbly_statusline = {
+                    'mode',
+                    'path',
+                    'signify',
+
+                    'divisor',
+                    'lsp_status.messages',
+                    'divisor',
+
+                    'lsp_status.diagnostics',
+                    'total_buffer_number',
+                    'filetype',
+                    'progress'
+                }
+                vim.g.bubbly_symbols = {
+                   path = {
+                      readonly = 'RO',
+                      unmodifiable = '',
+                      modified = '+',
+                   },
+                   signify = {
+                      added = '+%s', -- requires 1 '%s'
+                      modified = ' %s', -- requires 1 '%s'
+                      removed = '-%s', -- requires 1 '%s'
+                   },
+                   lsp_status = {
+                     diagnostics = {
+                        error = ' %d',
+                        warning = ' %d',
+                        hint = ' %d',
+                        info = ' %d',
+                     },
+                  },
+
+                }
+            end
+        }
+    -- File explorer
+        use { 'kyazdani42/nvim-tree.lua',
+            config = function()
+                --vim.g.nvim_tree_auto_open = 1
+            end
+        }
+        use { 'nvim-telescope/telescope.nvim',
+            requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+        }
+    -- Vim script plugins ------------------
+    -- Spell checker
+        use { 'kamykn/spelunker.vim',
+            config = function()
+                vim.cmd('set nospell')
+            end
+        }
+    -- Fuzzy finder
+       use { 'junegunn/fzf.vim',
+            config = function()
+                vim.g.fzf_command_prefix = 'Fzf'
+            end,
+            requires = { 'junegunn/fzf' }
+        }
+    -- -----------------Haven't refactored yet
+        use 'preservim/nerdcommenter'
+        use 'mhinz/vim-startify'
+        -- Git plugin
+        use 'tpope/vim-fugitive'
+        -- Indent line display
+        use 'iamcco/markdown-preview.vim'
+    end
+    )
+end
+plugins_install()
--- lua/plugins/init.lua
+++
@@ -1,5 +0,0 @@
-require('plugins.packer_bootstrap')
-require('plugins.install')
-require('plugins.lsp')
-require('plugins.mappings')
-require('plugins.setup')
