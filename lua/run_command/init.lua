c23519df47a0721836c1f3a0dbac615dcdd70f28
Sergei Kuznetsov <Sergei.Kuznetsov@Squarepoint-Capital.com>
2023-01-12 16:47:30 +0000

    Continue to write run_command

--- lua/run_command/init.lua
+++ lua/run_command/init.lua
@@ -5,31 +5,104 @@
 -- Read commands from some file and be able to choose from it or run something new
 local M = {}
 
-local DEFAULT_HEIGHT = 10
+local previousArgs = {
+    command = nil,
+    cwd = nil
+}
 
-local previousArgs = {}
 local runningJob = nil
+local quickfixBufferId = vim.fn.getqflist({ qfbufnr = 1 }).qfbufnr
+
 
 local Job = require 'plenary.job'
 
-local function run_command()
+local settings = {
+    window_height = 10,
+    shell = 'bash'
+}
+
+local function ask_for_cmd()
+    local cmd = nil
+    vim.ui.input({
+        prompt = 'Command: ',
+        default = previousArgs.command,
+        complete = 'shellcmd',
+    }, function(input) cmd = input end)
+    return cmd
+end
+
+local function ask_for_dir()
+    local dir = nil
+    vim.ui.input({
+        prompt = 'Directory to run: ',
+        default = previousArgs.cwd or vim.fn.getcwd(),
+        complete = 'dir',
+    }, function(input) dir = input end)
+    return dir
+end
+
+local function show_qf_list()
+    vim.cmd.copen()
+end
+
+local function writeToQf(data)
+    vim.schedule_wrap(function()
+        local start = vim.api.nvim_buf_line_count(quickfixBufferId)
+        vim.api.nvim_buf_set_option(quickfixBufferId, 'modifiable', true)
+        vim.api.nvim_buf_set_lines(quickfixBufferId, start, -1, false, { data })
+        vim.api.nvim_buf_set_option(quickfixBufferId, 'modifiable', false)
+    end)
+end
+
+function M.run_command(cmd, dir)
     if runningJob ~= nil then
         vim.notify('Job is already running', vim.log.levels.INFO)
         return
     end
-    cmd = vim.ui.input({
-        prompt = 'Command: ',
-        complete = 'shellcmd',
-    }, function(input)
-        return vim.split ' '
-    end)
+
+    cmd = cmd or ask_for_cmd()
+    dir = dir or ask_for_dir()
+
     runningJob = Job:new {
-        command = cmd[1],
-        args = {table.unpack(cmd, 2)},
+        command = settings.shell,
+        args = { '-c', cmd },
         cwd = dir,
-        on_stdout = function() end,
-        on_stderr = function() end,
+        on_start = function()
+            vim.notify('starting')
+            writeToQf('Running ' .. cmd)
+            show_qf_list()
+        end,
+        on_stdout = function(err, data)
+            vim.notify('on_stdout')
+            if err then
+                writeToQf(err)
+            end
+            if data then
+                writeToQf(data)
+            end
+        end,
+        on_stderr = function(err, data)
+            vim.notify('on_stderr')
+            if err then
+                writeToQf(err)
+            end
+            if data then
+                writeToQf(data)
+            end
+        end,
+        on_exit = function(code, signal)
+            vim.notify('on_exit')
+
+            if signal then
+                writeToQf('Command ' .. cmd .. ' finished with signal ' .. signal)
+            else
+                writeToQf('Command ' .. cmd .. ' finished with code ' .. code)
+            end
+        end
     }
+    runningJob:start()
+    previousArgs.command = cmd
+    previousArgs.cwd = dir
 end
 
 return M
