ecbd3dd9fbb07f9c5fe8e625d6af717fa383fbed
Sergey <kuzzz99@gmail.com>
2025-10-19 21:08:31 +0100

    Improve zk config

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,8 +2,7 @@ vim.opt.conceallevel = 0
 vim.opt_local.spell = true
 vim.opt_local.spelllang = 'en,ru'
 
-local file_path = vim.fn.expand '%:p'
-if require('zk.util').notebook_root(file_path) ~= nil then
+if require('zk.util').notebook_root(vim.fn.expand '%:p') ~= nil then
   local nmap_util = require('utils').nmap
   local nmap = function(lhs, rhs)
     nmap_util(lhs, rhs, { buffer = true })
@@ -24,4 +23,21 @@ if require('zk.util').notebook_root(file_path) ~= nil then
   nmap('<leader>tg', function()
     Snacks.picker.grep { dirs = { vim.fn.expand '%:h' } }
   end)
+
+  vim.api.nvim_buf_create_user_command(0, 'ZkDelete', function()
+    local current_file = vim.fn.expand '%:t'
+    if vim.fn.confirm('Delete file ' .. current_file, '&Yes\n&No', 2) == 1 then
+      vim.system({ 'trash', vim.fn.expand '%' }, {}, function(obj)
+        if obj.code ~= 0 then
+          vim.notify(
+            'Error deleting file ' .. current_file .. ': ' .. obj.stderr,
+            vim.log.levels.ERROR
+          )
+        else
+          vim.notify('Deleted ' .. current_file)
+          vim.schedule(function() Snacks.bufdelete() end)
+        end
+      end)
+    end
+  end, {})
 end
--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -1,4 +1,4 @@
-local notes_dir = vim.env.HOME .. '/Documents/zk'
+local notes_dir = vim.fn.resolve(vim.env.HOME .. '/Documents/zk')
 
 return {
   'zk-org/zk-nvim',
@@ -19,7 +19,23 @@ return {
           sort = { 'modified' },
         }
       end,
-      desc = 'Search in Obsidian',
+      desc = 'Search in Zk',
+    },
+    {
+      '<leader>zn',
+      function()
+        require('zk.commands').get 'ZkNew' { notebook_path = notes_dir }
+      end,
+    },
+    {
+      '<leader>zd',
+      function()
+        require('zk.commands').get 'ZkNew' {
+          notebook_path = notes_dir,
+          dir = notes_dir .. '/Daily',
+          group = 'daily',
+        }
+      end,
     },
   },
 }
